572fe3c832b19aa922130eae61e9016c4434feb5
Anastasia Plotnikova <nastaploton@gmail.com>
2023-01-29 17:31:06 +0300

    внесли метод в Task 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -10,8 +10,19 @@
 int number = new Random().Next(10, 100); // последнее число не входит в диапозон
 Console.WriteLine($"Случайное число из отрезка 10 -99 => {number}");
 
-int firstDigit = number / 10; // 7
-int secondDigit = number % 10; // 8
+// int firstDigit = number / 10; // 7
+// int secondDigit = number % 10; // 8
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10; // 7
+    int secondDigit = num % 10; // 8
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
