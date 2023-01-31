d0d1a18de055cf06f2de1e05457ed5099617fa59
Anastasia Plotnikova <nastaploton@gmail.com>
2023-01-31 10:41:53 +0300

    Изменения в Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,14 +3,14 @@
 // возможных координат точек в этой четверти (x и y).
 
 
-Console.WriteLine("Введите номер плоскости");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер плоскости: ");
+string number = Console.ReadLine(); //int number = Convert.ToInt32(Console.ReadLine());
 
 string quarter= Quarter(number);
 
-Console.WriteLine($"{quarter}");
+Console.WriteLine(quarter);  //Console.WriteLine($"{quarter}");
 
-string Quarter(int num)
+string Quarter(string num) // (int num)
 {
     // if(xc > 0 && yc > 0) return 1;
     // if(xc < 0 && yc > 0) return 2;
@@ -18,9 +18,18 @@ string Quarter(int num)
     // if(xc < 0 && yc > 0) return 4;
     // return 0;
 
-    if(num == 1) return "x > 0 и y > 0";
-    if(num == 2) return "x < 0 и y > 0";
-    if(num == 3) return "x > 0 и y < 0";
-    if(num == 4) return "x < 0 и y > 0";
-    return "Введены не кореретные координаты";
+    // if(num == 1) return "x > 0 и y > 0";
+    // if(num == 2) return "x < 0 и y > 0";
+    // if(num == 3) return "x > 0 и y < 0";
+    // if(num == 4) return "x < 0 и y > 0";
+    // return "Введены неккоректные координаты";
+
+    switch(num)
+    {
+        case "1" : return "Допустимые координаты: x > 0 и y > 0";  
+        case "2" : return "Допустимые координаты: x < 0 и y > 0"; 
+        case "3" : return "Допустимые координаты: x > 0 и y < 0"; 
+        case "4" : return "Допустимые координаты: x < 0 и y > 0"; 
+        default: return "Неккоректный ввод";
+    }
 }
