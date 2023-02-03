22d402f1f30709900827fc91dd36912278c7b953
Anastasia Plotnikova <nastaploton@gmail.com>
2023-02-03 12:26:25 +0300

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// 19.Напишите программу, которая 
+// 1 . принимает на вход пятизначное число и 
+// 2. проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool Palindrome(string num)
+{
+    return(num[0] == num[4] || num[1] == num[3]);
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+// bool result = Palindrome(number);
+
+if(number .Length == 5)
+{
+    bool result = Palindrome(number);
+    Console.WriteLine($"{result}"); 
+} 
+else Console.WriteLine("Введено неверное число.");
