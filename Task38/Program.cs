83a81c4d34047bcbda7ac3a14719dc3fe80712a8
Anastasia Plotnikova <nastaploton@gmail.com>
2023-02-09 21:03:55 +0300

    Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,3 +2,52 @@
 // Найдите разницу между максимальным и минимальным
 // элементами массива.
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+    }
+    return array;
+}
+
+void PrintArrayDouble(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+double MinMaxArrayElement(double[] array)
+{
+    double min = array[0];
+    double max = default;
+    double difference = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        difference = max - min;
+    }
+    return Math.Round(difference, 2);
+}
+ 
+double[] arr = CreateArrayRndDouble(5, 0, 100);
+PrintArrayDouble(arr);
+
+double minMaxArrayElement = MinMaxArrayElement(arr);
+Console.WriteLine($" -> {minMaxArrayElement}");
