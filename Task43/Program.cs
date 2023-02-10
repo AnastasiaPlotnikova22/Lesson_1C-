fd6cba3884d0475a66c844118c73da64b6a1710b
Anastasia Plotnikova <nastaploton@gmail.com>
2023-02-10 15:59:09 +0300

    Изменения Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -13,8 +13,23 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b1 - b2)/(k2 - k1);
-double y = k2 * x + b2;
+// double x = (b1 - b2)/(k2 - k1);
+// double y = k2 * x + b2;
 
-Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+//Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
+double PointX(double ba1 ,double ka1 ,double ba2 ,double ka2)
+{
+    double x = (b1 - b2)/(k2 - k1);
+    return x;
+}
+
+double PointY(double ba1 ,double ka1 ,double ba2 ,double ka2 ,double pointX)
+{
+    double y = k2 * pointX + b2;
+    return y;
+}
+
+double pointX = PointX( b1 , k1 , b2 , k2);
+double pointY = PointY( b1 , k1 , b2 , k2, pointX);
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {pointX}, Y: {pointY}");
