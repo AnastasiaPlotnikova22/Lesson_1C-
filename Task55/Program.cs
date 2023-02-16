34c30bfd074549e33dfd3c12188d588a11a61f5e
Anastasia Plotnikova <nastaploton@gmail.com>
2023-02-16 13:20:54 +0300

    Изменения #2 Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -55,3 +55,40 @@ if(matrix.GetLength(0) == matrix.GetLength(1))
     PrintMatrix(matrix);
 }
 else Console.WriteLine("Число строк и столбцов несовпадает!");
+
+
+
+// int[,] GenerateMatrix(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// string PrintMatrix(int[,] matrix)
+// {
+//     string str = string.Empty;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         str += "[";
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j == matrix.GetLength(1) - 1) str += $" {matrix[i, j],3} ";
+//             else str += $" {matrix[i, j],3}, ";
+//         }
+//         str += "]\n";
+//     }
+//     return str;
+
+// }
+
+// int[,] ChangeRowsOnColumns(int[,] matrix)
+// {
+// int[,] temporaryMatrix = new int[matrix]
