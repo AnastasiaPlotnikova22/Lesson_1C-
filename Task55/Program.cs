e9170f57b39fa616385694c5a1605ea1e7e98dcb
Anastasia Plotnikova <nastaploton@gmail.com>
2023-02-16 13:09:06 +0300

    Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,59 @@
+﻿// 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Replece(int[,] matrix)
+{
+   int[,] tmpArray = new int[matrix.GetLength(0), matrix.GetLength(1)] ;
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        int tmp = matrix[i, j];
+        tmpArray[j,i] = matrix[i, j];
+        matrix[i, j] = tmp;
+      }
+   }
+   return tmpArray;
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+if(matrix.GetLength(0) == matrix.GetLength(1)) 
+{
+    int[,] newMatrix = Replece(matrix);
+    PrintMatrix(matrix);
+}
+else Console.WriteLine("Число строк и столбцов несовпадает!");
